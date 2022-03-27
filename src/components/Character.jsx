1796a38f56b7a37ab399e9b9d32276c1e3ca98b9
veronicapava <veronicapava@gmail.com>
2022-03-27 00:56:46 -0500

    cambio menor. adiciona funcion handleClick

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
 
-const Character = ({ character, key }) => {
+const Character = ({ character }) => {
   const [liked, setLiked] = useState(false);
+  const [likedCharacters, saveLikedCharacter] = useState({});
+
+  const handleClick = (char) => {
+    setLiked(!liked);
+    !liked ? saveLikedCharacter(char) : saveLikedCharacter({});
+  };
 
   return (
-    <div key={key} className="col-4 mb-4">
+    <div className="col-4 mb-4">
       <div className="card">
         <img src={character.image} alt="" />
       </div>
       <div className="card-body">
         <h3>{character.name}</h3>
         <div>
-          <button className="button" onClick={() => setLiked(!liked)}>
+          <button className="button" onClick={() => handleClick(character)}>
             {liked ? "❤️" : "🤍"}
           </button>
         </div>
@@ -23,20 +29,3 @@ const Character = ({ character, key }) => {
 };
 
 export default Character;
-
-{
-  /* <h3>{character.liked}</h3> */
-}
-
-{
-  /* <button
-                className="button"
-                onClick={() => {
-                  console.log("changin on character id " + character.id);
-                  character.liked = !character.liked;
-                  console.log("New value " + character.liked);
-                }}
-              >
-                {character.liked ? "❤️" : "🤍"}
-              </button> */
-}
